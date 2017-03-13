3eeef48f46611c0a765ca64157f3fe777be9c9ac
aiden <aidenscott12@googlemail.com>
2017-03-13 19:26:48 +0100

    show label whwn you voted. ad helper methods for this

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,8 @@ const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
     <div>
-      <Voting pair={pair} vote={(entry) => {console.log(entry)}} />
+      {/*<Voting pair={pair} vote={(entry) => {console.log(entry)}} />*/}
+      <Voting pair={pair} hasVoted={"Trainspotting"} /> 
     </div>,
     document.getElementById('app')
 );
