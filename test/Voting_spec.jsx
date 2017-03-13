b18cee98109a8b906e034f88b51a524d598058f3
aiden <aidenscott12@googlemail.com>
2017-03-13 19:11:22 +0100

    all tests on Voting.tsx pass from aeroplane

--- test/Voting_spec.jsx
+++ test/Voting_spec.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   scryRenderedDOMComponentsWithTag,
-  renderIntoDocument
+  renderIntoDocument,
+  Simulate
 } from 'react-addons-test-utils';
-import Voting from '../../src/components/Voting';
+import Voting from '../src/components/Voting.jsx';
 import {expect} from 'chai';
 
 describe('Voting', () => {
@@ -15,10 +16,11 @@ describe('Voting', () => {
     );
 
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    console.log(buttons[0].methods);
 
     expect(buttons.length).to.equal(2);
-    expect(buttons[0].textContents).to.equal('Trainspotting');
-    expect(buttons[1].textContents).to.equal('28 Days Later');
+    expect(buttons[0].textContent).to.equal('Trainspotting');
+    expect(buttons[1].textContent).to.equal('28 Days Later');
 
 
   });
@@ -34,7 +36,10 @@ describe('Voting', () => {
     );
 
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
 
-
+    expect(votedWith).to.equal('Trainspotting'); 
+  });
 
 });
+
